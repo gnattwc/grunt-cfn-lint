75e04a84b6c0bb1ffc020f213a4ed6b1488cbbb1
chris.tang@philips.com <chris.tang@philips.com>
2018-11-17 14:44:25 -0800

    took out grunt-clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,11 +23,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
     // Configuration to be run (and then tested).
     cfn_lint: {
       byExtension: {
@@ -64,12 +59,11 @@ module.exports = function (grunt) {
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'cfn_lint', 'nodeunit']);
+  grunt.registerTask('test', ['cfn_lint', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
